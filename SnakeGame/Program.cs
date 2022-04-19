46e7ba1abe45202066bf92551ab3129b211e8de4
bondarenkoigor <Bond_fh54@student.itstep.org>
2022-04-19 20:30:21 +0300

    Revert "PrintMap() fixed"
    This reverts commit 7cdd2ee1a4bb17242f0b9b4aeb24f56576412475.

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -14,7 +14,6 @@ namespace SnakeGame
             if (speed == 1) speed = 500;
             else if (speed == 3) speed = 100;
             else  speed = 300;
-            Console.Clear();
             return new Game(x, y, speed);
 
         }
